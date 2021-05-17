4140f40b6ee74ff4c35e916bfc039f5a289033c0
Denzel Code <denzelcodedev@gmail.com>
2021-05-16 23:27:33 -0400

    chore: production environment updated

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -2,53 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 // Angular modules
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCardModule } from '@angular/material/card';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatDialogModule } from '@angular/material/dialog';
 import { MatGridListModule } from '@angular/material/grid-list';
-import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { MatListModule } from '@angular/material/list';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatSortModule } from '@angular/material/sort';
-import { MatTableModule } from '@angular/material/table';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatTooltipModule } from '@angular/material/tooltip';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
   exports: [
-    BrowserAnimationsModule,
-    MatAutocompleteModule,
-    MatButtonModule,
-    MatButtonToggleModule,
-    MatDialogModule,
     MatInputModule,
-    MatListModule,
-    MatSelectModule,
-    MatSortModule,
-    MatTableModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
     MatCardModule,
-    MatTabsModule,
-    MatSlideToggleModule,
-    MatMenuModule,
-    MatRadioModule,
-    MatCheckboxModule,
-    MatTooltipModule,
-    MatPaginatorModule,
     MatGridListModule,
-    MatIconModule,
-    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatListModule,
   ],
 })
 export class AngularMaterialModule {}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  api: '/api',
+  socket: 'http://54.196.222.122:3000',
 };
