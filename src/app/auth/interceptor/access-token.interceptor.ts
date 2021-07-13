e42c73ab95d0498d9cc0cfc99918ad02265bbcce
Denzel Code <denzelcodedev@gmail.com>
2021-07-13 10:31:41 -0400

    feat: refresh token interceptor added

--- src/app/auth/interceptor/access-token.interceptor.ts
+++ src/app/auth/interceptor/access-token.interceptor.ts
@@ -6,7 +6,7 @@ import {
   HttpInterceptor,
   HttpErrorResponse,
 } from '@angular/common/http';
-import { Observable, of, throwError } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { tap, take, mergeMap, catchError } from 'rxjs/operators';
 import { AuthService } from '../service/auth.service';
 
