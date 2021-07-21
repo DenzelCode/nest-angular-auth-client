94ef8dac48eabeb5ec2e5b13abb084dfb890bd82
Denzel Code <denzelcodedev@gmail.com>
2021-07-21 03:26:56 -0400

    chore: social library added

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  FacebookLoginProvider,
+  GoogleLoginProvider,
+  SocialAuthServiceConfig,
+  SocialLoginModule,
+} from 'angularx-social-login';
 
 @NgModule({
   declarations: [],
-  providers: [],
-  imports: [CommonModule],
-  exports: [],
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider('clientId'),
+          },
+          {
+            id: FacebookLoginProvider.PROVIDER_ID,
+            provider: new FacebookLoginProvider('535472397651204'),
+          },
+        ],
+      } as SocialAuthServiceConfig,
+    },
+  ],
+  imports: [CommonModule, SocialLoginModule],
+  exports: [SocialLoginModule],
 })
 export class AuthModule {}
