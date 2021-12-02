02ebf1d4114b0c864e1686c6878631c7cf9ecc53
Denzel Code <denzelcodedev@gmail.com>
2021-12-01 21:55:01 -0500

    style: right colors for chat

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,24 @@
+// export const environment = {
+//   production: false,
+//   api: 'http://localhost:3000',
+//   socket: 'http://localhost:3000',
+//   apps: {
+//     facebook: '513024756601831',
+//     google:
+//       '331672215174-0hlpm8fhjphiou05ovsd82vglor401ct.apps.googleusercontent.com',
+//     apple: {
+//       clientId: 'nest-auth.ubbly.club',
+//       redirectUri: 'https://nest-auth.ubbly.club/',
+//     },
+//   },
+// };
+
 export const environment = {
-  production: false,
-  api: 'http://localhost:3000',
-  socket: 'http://localhost:3000',
+  production: true,
+  api: 'https://nest-auth.ubbly.club/api',
+  socket: 'https://nest-auth.ubbly.club/',
   apps: {
-    facebook: '513024756601831',
+    facebook: '535472397651204',
     google:
       '331672215174-0hlpm8fhjphiou05ovsd82vglor401ct.apps.googleusercontent.com',
     apple: {
